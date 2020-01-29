17cf06af8bb47c21f1a2aff0b310b8ee583a2553
Yair <>
2020-01-29 20:44:15 +0200

    Adds tests for the Event object.

--- src/test/kotlin/za/co/tyche/domain/EventTest.kt
+++ src/test/kotlin/za/co/tyche/domain/EventTest.kt
@@ -1,11 +1,41 @@
 package za.co.tyche.domain
 
-import io.kotlintest.specs.StringSpec
+import io.kotlintest.matchers.types.shouldNotBeNull
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldThrow
+import io.kotlintest.specs.FreeSpec
+import za.co.tyche.stub.FixedDates
 
-class EventTest : StringSpec() {
+class EventTest : FreeSpec({
 
-    init {
+    "init_startDateAfterEndDate_throwsValidationError" {
+        val errorMessage = shouldThrow<IllegalArgumentException> {
+            Event(description = "foo", startDate = FixedDates.nowPlus10Days, endDate = FixedDates.now)
+        }.message
+
+        errorMessage.shouldBe("Start date must be before end date. StartDate: [${FixedDates.nowPlus10Days}], EndDate: [${FixedDates.now}]")
     }
 
-}
+    "init_blankDescription_throwsValidationError" - {
+        listOf(
+                "",
+                "        ",
+                "\t\t\n\t\n"
+        )
+                .map { invalidDescription ->
+                    val errorMessage = shouldThrow<IllegalArgumentException> {
+                        Event(description = invalidDescription, startDate = FixedDates.now, endDate = FixedDates.nowPlus10Days)
+                    }.message
+
+                    errorMessage.shouldBe("Description cannot be empty")
+                }
+    }
+
+    "init_validParameters_buildsWithoutIssues"  {
+        val event = Event(description = "foo bar", startDate = FixedDates.now, endDate = FixedDates.nowPlus10Days)
+
+        event.shouldNotBeNull()
+    }
+})
+
+
